2b856b55959021b5b508f68611ffc39148b754aa
Sahim-Nazif <nsahim@my.centennialcollege.ca>
2018-09-16 20:03:20 -0400

    Created Scanner class to get user input

--- Calculator/src/TempratureConvertor.java
+++ Calculator/src/TempratureConvertor.java
@@ -1,8 +1,11 @@
-
+import java.util.Scanner;
 public class TempratureConvertor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	Scanner input=new Scanner(System.in);
+	
+	
 
 	}
 
